6f61261ea3506daac368003087f156b457a561a7
saikumar <venkatasaikumarraju.bahatam@gmail.com>
2023-10-15 16:47:19 +0530

    first commit

---
+++ src/components/Works/works.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './works.css';
+
+
+const Works=()=>{
+    return(
+        <section id ='works'>
+            <h2 className="workstitle">My Portfolio</h2>
+            <span className="worksDesc"> I was seeking for a position with a forward-thinking company where my programming knowledge, analytical skills, and creativity could be put to use to develop creative solutions and advance the firm.
+
+I'm developing my career, working for a changing firm, and the world.
+
+while obtaining beneficial experience
+
+.</span>
+            <div className="worksImgs">
+            </div>
+            <button className= "worksBtn">See More</button>
+        </section>
+    );
+}
+
+export default Works
